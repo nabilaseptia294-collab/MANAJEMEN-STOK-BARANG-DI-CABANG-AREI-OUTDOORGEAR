8e99d300ad4d1b3df135a1d031b469aec74057e3
aulliaSyakira <auliasyakura1103@gmail.com>
2025-12-23 12:13:32 +0700

    fix stok controller

--- app/Http/Controllers/StokController.php
+++ app/Http/Controllers/StokController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produk;
+use Illuminate\Http\Request;
 
 class StokController extends Controller
 {
     public function index()
     {
         $stok = Stok::with('produk')->get();
-
         return view('stok.index', compact('stok'));
+    }
+
+    public function show($id)
+    {
+        $stok = Stok::with('produk')->where('id_produk', $id)->firstOrFail();
+        return view('stok.show', compact('stok'));
     }
 }
